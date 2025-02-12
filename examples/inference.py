386fef507eb90dab91ec9857a15043d77cec3606
Вячеслав Чертан <slavakemdev@gmail.com>
2025-02-12 17:02:15 +0300

    Update inference example

--- examples/inference.py
+++ examples/inference.py
@@ -5,9 +5,10 @@ from model.predictor import Predictor
 predictor = Predictor(6, input("Model checkpoint path: "))
 waveform, sample_rate = torchaudio.load(input("Sound file location: "))
 
-mid = waveform.shape[1]
-offset = 5 * sample_rate // 2
+waveform = waveform[0]
+step = 5 * sample_rate
 
-frag = waveform[:, mid-offset:mid+offset]
-
-print(predictor.predict(frag, sample_rate))
+for start in range(0, waveform.shape[0], step):
+    frag = waveform[start:start+step]
+    pred = predictor.predict(frag, sample_rate)
+    print(int(pred.argmax(dim=0)), pred)
