f3611169081202995792b4b2666fac7b0dd6cf00
Вячеслав Чертан <slavakemdev@gmail.com>
2025-02-13 15:30:30 +0300

    Fix classifier

--- model/classifier.py
+++ model/classifier.py
@@ -6,13 +6,18 @@ class Classifier:
     def __init__(self, model_path="best_checkpoint.pth"):
         self.predictor = Predictor(6, model_path)
 
-    def get_probabilities(self, array, sampling_rate, window_length=5):
+    def get_probabilities(self, array: torch.Tensor, sampling_rate: int, window_length: int = 5) -> torch.Tensor:
         n_samples = int(array.shape[1])
         window = window_length * sampling_rate
-        predictions = \
-            [self.predictor.predict(array[:, start:min(start + window, n_samples)], sampling_rate) * (
-                        (min(start + window, n_samples) - start) / window) for start in
-             range(0, n_samples, window)]
+
+        predictions: list[torch.Tensor] = []
+        for start in range(0, n_samples, window):
+            end = min(start + window, n_samples)
+            pred = self.predictor.predict(array[:, start:end], sampling_rate)
+            pred *= (end - start) / window
+
+            predictions.append(pred)
+
         result = sum(predictions)
         result /= sum(result)
         return result
